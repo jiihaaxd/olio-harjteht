1432d4da65beab2ef3669b5e4c8c7fb2bdf0b2fe
jiihaaxd <33057403+jiihaaxd@users.noreply.github.com>
2017-12-21 02:49:38 +0200

    Add files via upload

---
+++ Mainclass.java
@@ -0,0 +1,25 @@
+package viikko7;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Mainclass extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Viikko7");
+        
+        Parent root = FXMLLoader.load(getClass().getResource("v7.fxml"));
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
